76fe0b04fe457a2f4f0e291ff9315799704ae294
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2024-07-20 16:29:50 +0900

    nextの設定修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
